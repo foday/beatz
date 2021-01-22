596c42027a737beb8845d0c26ca7ce129035d1f2
foday <foday_s@yahoo.com>
2021-01-22 15:50:30 -0600

    makemigrations, migrate and add some codes to frontend>views.py file and frontend>urls.py file
    1. music_controller>python manage.py makemigrations
    2. music_controller>python manage.py migrate
    (note: I forgot to add the codes below to the frontend>urls.py file and the 2nd one to frontend>views.py file)
    from django.urls import path
    from .views import main
    
    urlpatterns = [
    path('', main),
    ]
    from django.shortcuts import render
    from django.http import HttpResponse
    
    # Create your views here.
    def main(request):
           return HttpResponse('This is the frontend')

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'frontend.apps.ApiConfig',
+    'frontend.apps.FrontendConfig',
     'rest_framework',
 ]
 
