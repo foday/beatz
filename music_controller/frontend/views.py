7f7f5b42416711e50fbf353d9fe7581675a6c7e4
foday <foday_s@yahoo.com>
2021-01-23 14:43:00 -0600

    Webpack & Babel: Integrating backend (Django, Django REST Framework API) and frontend (React JS, Material ui)

--- music_controller/frontend/views.py
+++ music_controller/frontend/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def main(request):
-    return HttpResponse("This is the frontend")
+def index(request, *args, **kwargs):
+    return render(request, "frontend/index.html")
