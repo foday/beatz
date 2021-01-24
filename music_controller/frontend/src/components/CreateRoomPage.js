37c735f3c03ab5de5718a8854835c144aceafaf2
foday <foday_s@yahoo.com>
2021-01-24 08:59:48 -0600

    Create 3 components and add code to those 3 components then add the 3 components to App.js

---
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import { render } from "react-dom";
+
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+render() {
+    return ( <p>This is the create room page</p>);
+    }
+}
