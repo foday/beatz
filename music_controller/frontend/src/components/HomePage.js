b4e7057c8dca43d05c3928092ff6c18be649ef8d
foday <foday_s@yahoo.com>
2021-01-24 10:12:48 -0600

    Homepage.js, Router, Route 'exact' path

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,5 +1,9 @@
 import { Component } from "react";
 import { render } from "react-dom";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-dom";
+
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -7,6 +11,13 @@ export default class HomePage extends Component {
     }
 
 render() {
-    return ( <p>This is the home page</p>);
+    return ( 
+    <Router>
+        <Switch>
+            <Route exact path='/'><p>This is the home page</p></Route>
+            <Route path='/join' component={RoomJoinPage}></Route>
+            <Route path='/create' component={CreateRoomPage}></Route>
+        </Switch>
+    </Router>);
     }
 }
